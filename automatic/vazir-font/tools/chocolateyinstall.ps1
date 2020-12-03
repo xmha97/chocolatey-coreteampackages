614ec221da4ab6a4c9f3049228fc33ca313e7345
Muhammad Hussein Ammari <xmha97@gmail.com>
2020-12-03 20:58:01 +0330

    Fixed some issues

--- automatic/vazir-font/tools/chocolateyinstall.ps1
+++ automatic/vazir-font/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $url        = "https://github.com/rastikerdar/vazir-font/releases/download/v$env
 $myChecksum = 'cb74f7db8257d783d6301c798ae2e10d25035aaf4d202fca17c2129578ab29c3'
 
 $packageArgs = @{
-  softwareName  = 'DejaVuSansMono Nerd Font'
+  softwareName  = 'Vazir font'
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url           = $url
@@ -16,27 +16,4 @@ $packageArgs = @{
 
 Install-ChocolateyZipPackage @packageArgs
 
-Install-ChocolateyFont "$toolsDir\Vazir.ttf"
-Install-ChocolateyFont "$toolsDir\Vazir-Thin.ttf"
-Install-ChocolateyFont "$toolsDir\Vazir-Medium.ttf"
-Install-ChocolateyFont "$toolsDir\Vazir-Light.ttf"
-Install-ChocolateyFont "$toolsDir\Vazir-Bold.ttf"
-Install-ChocolateyFont "$toolsDir\Vazir-Black.ttf"
-Install-ChocolateyFont "$toolsDir\Farsi-Digits\Vazir-Thin-FD.ttf"
-Install-ChocolateyFont "$toolsDir\Farsi-Digits\Vazir-Medium-FD.ttf"
-Install-ChocolateyFont "$toolsDir\Farsi-Digits\Vazir-Light-FD.ttf"
-Install-ChocolateyFont "$toolsDir\Farsi-Digits\Vazir-FD.ttf"
-Install-ChocolateyFont "$toolsDir\Farsi-Digits\Vazir-Bold-FD.ttf"
-Install-ChocolateyFont "$toolsDir\Farsi-Digits\Vazir-Black-FD.ttf"
-Install-ChocolateyFont "$toolsDir\Farsi-Digits-Without-Latin\Vazir-Thin-FD-WOL.ttf"
-Install-ChocolateyFont "$toolsDir\Farsi-Digits-Without-Latin\Vazir-Medium-FD-WOL.ttf"
-Install-ChocolateyFont "$toolsDir\Farsi-Digits-Without-Latin\Vazir-Light-FD-WOL.ttf"
-Install-ChocolateyFont "$toolsDir\Farsi-Digits-Without-Latin\Vazir-FD-WOL.ttf"
-Install-ChocolateyFont "$toolsDir\Farsi-Digits-Without-Latin\Vazir-Bold-FD-WOL.ttf"
-Install-ChocolateyFont "$toolsDir\Farsi-Digits-Without-Latin\Vazir-Black-FD-WOL.ttf"
-Install-ChocolateyFont "$toolsDir\Without-Latin\Vazir-WOL.ttf"
-Install-ChocolateyFont "$toolsDir\Without-Latin\Vazir-Thin-WOL.ttf"
-Install-ChocolateyFont "$toolsDir\Without-Latin\Vazir-Medium-WOL.ttf"
-Install-ChocolateyFont "$toolsDir\Without-Latin\Vazir-Light-WOL.ttf"
-Install-ChocolateyFont "$toolsDir\Without-Latin\Vazir-Bold-WOL.ttf"
-Install-ChocolateyFont "$toolsDir\Without-Latin\Vazir-Black-WOL.ttf"
+ls $toolsDir\*.ttf -Recurse | foreach {Install-ChocolateyFont $_}
