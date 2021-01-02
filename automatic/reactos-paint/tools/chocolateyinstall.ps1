05af885afc6abba2a46c492b4dddd9af46f61fe6
Muhammad Hussein Ammari <xmha97@gmail.com>
2021-01-02 17:00:06 +0330

    Add reactos-paint package

---
+++ automatic/reactos-paint/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# Install start menu shortcut
+$programs = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\ReactOS"
+$shortcutFilePath = Join-Path $programs "Paint.lnk"
+$targetPath = Join-Path $toolsDir "mspaint.exe"
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
---
+++ automatic/reactos-paint/tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+﻿$ErrorActionPreference = 'Stop'
+# Remove start menu shortcut
+$programs = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\ReactOS"
+$shortcutFilePath = Join-Path $programs "Paint.lnk"
+if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }
