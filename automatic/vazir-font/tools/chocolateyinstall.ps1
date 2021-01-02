428d32274a9fb43b330f316271ecb8a3fb2fd6ae
Muhammad Hussein Ammari <xmha97@gmail.com>
2021-01-02 15:35:35 +0330

    Updated Checksum

--- automatic/vazir-font/tools/chocolateyinstall.ps1
+++ automatic/vazir-font/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $myVersion  = [system.String]::Join('.', $env:ChocolateyPackageVersion.Split('.')[0..2])
 $url        = "https://github.com/rastikerdar/vazir-font/releases/download/v$env:ChocolateyPackageVersion/vazir-font-v$env:ChocolateyPackageVersion.zip"
-$myChecksum = 'cb74f7db8257d783d6301c798ae2e10d25035aaf4d202fca17c2129578ab29c3'
+$myChecksum = '3a45738af972306a40a2fa8239a359c239b4efbf614bcf89a1022c533a1666c8'
 
 $packageArgs = @{
   softwareName  = 'Vazir font'
