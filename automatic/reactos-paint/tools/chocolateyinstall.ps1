dbe847c4f90287b0ce7fd3b74f12709b7fdaa6ce
Muhammad Hussein Ammari <xmha97@gmail.com>
2021-01-03 10:36:45 +0330

    Updated reactos-paint to v0.4.15

--- automatic/reactos-paint/tools/chocolateyinstall.ps1
+++ automatic/reactos-paint/tools/chocolateyinstall.ps1
@@ -4,5 +4,12 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 # Install start menu shortcut
 $programs = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\ReactOS"
 $shortcutFilePath = Join-Path $programs "Paint.lnk"
-$targetPath = Join-Path $toolsDir "mspaint.exe"
+if ([Environment]::Is64BitOperatingSystem)
+{
+    $targetPath = Join-Path $toolsDir "x64\mspaint.exe"
+}
+else
+{
+    $targetPath = Join-Path $toolsDir "x86\mspaint.exe"
+}
 Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
