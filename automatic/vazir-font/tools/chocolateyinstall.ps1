e6f2a221a2a84ddcd23cb93d259b99adcb5dbfe1
Muhammad Hussein Ammari <xmha97@gmail.com>
2021-01-15 02:48:10 +0330

    Updated vazir-font to v27.2.0

--- automatic/vazir-font/tools/chocolateyinstall.ps1
+++ automatic/vazir-font/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $myVersion  = [system.String]::Join('.', $env:ChocolateyPackageVersion.Split('.')[0..2])
 $url        = "https://github.com/rastikerdar/vazir-font/releases/download/v$env:ChocolateyPackageVersion/vazir-font-v$env:ChocolateyPackageVersion.zip"
-$myChecksum = '3a45738af972306a40a2fa8239a359c239b4efbf614bcf89a1022c533a1666c8'
+$myChecksum = '29dd19de15983dd3b027f9760ef47c262e95d9e006326adc2c486245d9395a41'
 
 $packageArgs = @{
   softwareName  = 'Vazir font'
