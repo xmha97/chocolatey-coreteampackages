53ea4495176995a23a9d8786d1eb58a73d709bd5
Muhammad Hussein Ammari <xmha97@gmail.com>
2021-01-21 01:41:02 +0330

    Update packages

--- automatic/ooniprobe-cli/tools/chocolateyInstall.ps1
+++ automatic/ooniprobe-cli/tools/chocolateyInstall.ps1
@@ -1,12 +1,22 @@
-$packageName    = 'ooniprobe-cli'
-$url            = 'https://github.com/ooni/probe-cli/releases/download/v3.0.7/ooniprobe_v3.0.7_windows_amd64.zip'
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$checksum       = '02d86a5a8494c63949cf81934ae2090ceae526bf54565489fee336cd10f546eb'
-$checksumType   = 'sha256'
+$ErrorActionPreference = 'Stop'
 
-Install-ChocolateyZipPackage `
-  -PackageName $packageName `
-  -Url64bit "$url" `
-  -UnzipLocation "$toolsDir" `
-  -Checksum64 $checksum `
-  -ChecksumType64 $checksumType
+$packageName    = 'ooniprobe-cli'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url            = "https://github.com/ooni/probe-cli/releases/download/v$env:ChocolateyPackageVersion/ooniprobe_v$env:ChocolateyPackageVersion_windows_386.zip"
+$url64          = "https://github.com/ooni/probe-cli/releases/download/v$env:ChocolateyPackageVersion/ooniprobe_v$env:ChocolateyPackageVersion_windows_amd64.zip"
+
+$checksum       = '6a859c8d24c1e025949c71a60363b1c4302683f648b758c5e6132b7029611665'
+$checksum64     = '4165c0c95a4fc138dcfe836ccf628660fd570ea8e01a1c07655012c85f7d7e93'
+
+$checksumType   = 'sha256'
+$checksumType64 = 'sha256'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
--- automatic/vazir-font/tools/chocolateyinstall.ps1
+++ automatic/vazir-font/tools/chocolateyinstall.ps1
@@ -1,7 +1,6 @@
 $ErrorActionPreference = 'Stop'
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$myVersion  = [system.String]::Join('.', $env:ChocolateyPackageVersion.Split('.')[0..2])
 $url        = "https://github.com/rastikerdar/vazir-font/releases/download/v$env:ChocolateyPackageVersion/vazir-font-v$env:ChocolateyPackageVersion.zip"
 $myChecksum = '29dd19de15983dd3b027f9760ef47c262e95d9e006326adc2c486245d9395a41'
 
