42a22f28e77ce15332badddf9033b43146329552
Muhammad Hussein Ammari <xmha97@gmail.com>
2021-01-21 04:04:22 +0330

    Fixed download issue

--- automatic/ooniprobe-cli/tools/chocolateyInstall.ps1
+++ automatic/ooniprobe-cli/tools/chocolateyInstall.ps1
@@ -2,9 +2,9 @@ $ErrorActionPreference = 'Stop'
 
 $packageName    = 'ooniprobe-cli'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-$url            = "https://github.com/ooni/probe-cli/releases/download/v$env:ChocolateyPackageVersion/ooniprobe_v$env:ChocolateyPackageVersion_windows_386.zip"
-$url64          = "https://github.com/ooni/probe-cli/releases/download/v$env:ChocolateyPackageVersion/ooniprobe_v$env:ChocolateyPackageVersion_windows_amd64.zip"
+$baseURL        = "https://github.com/ooni/probe-cli/releases/download/v$env:ChocolateyPackageVersion/ooniprobe_v" + "$env:ChocolateyPackageVersion"
+$url            = $baseURL + "_windows_386.zip"
+$url64          = $baseURL + "_windows_amd64.zip"
 
 $checksum       = '6a859c8d24c1e025949c71a60363b1c4302683f648b758c5e6132b7029611665'
 $checksum64     = '4165c0c95a4fc138dcfe836ccf628660fd570ea8e01a1c07655012c85f7d7e93'
